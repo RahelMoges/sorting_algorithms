fcc7d213c90da2001e69412e1cc307636dcb6669
RahelMoges <rahelmoges192@gmail.com>
2023-01-28 13:53:37 -0800

    insertion sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,7 +9,6 @@
 void swap_ints(int *a, int *b)
 {
 int tmp;
-
 tmp = *a;
 *a = *b;
 *b = tmp;
@@ -28,20 +27,18 @@ void selection_sort(int *array, size_t size)
 {
 int *min;
 size_t i, j;
-
 if (array == NULL || size < 2)
 return;
-
 for (i = 0; i < size - 1; i++)
 {
 min = array + i;
 for (j = i + 1; j < size; j++)
 min = (array[j] < *min) ? (array + j) : min;
 if ((array + i) != min)
-	{
-	  swap_ints(array + i, min);
-	  print_array(array, size);
-	}
+{
+swap_ints(array + i, min);
+print_array(array, size);
+}
 }
 }
 
